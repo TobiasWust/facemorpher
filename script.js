d7fa5a1e4d3b7c851eb2adc0155a81773fdbe3a1
Tobias Wust <kontakt@tobiaswust.de>
2019-08-19 00:22:21 +0200

    zoom, alignment

--- script.js
+++ script.js
@@ -17,10 +17,13 @@ gif.render();
 }
 
 const start = async () => {
+  let lefteye;
+  let eyedistance;
   imageUpload.addEventListener('change', async () => {
     [...imageUpload.files].forEach(async file => {
       const image = await faceapi.bufferToImage(file)
       const canvas = faceapi.createCanvasFromMedia(image)
+      const canvas2 = faceapi.createCanvasFromMedia(image)
       
       const displaySize = { width: image.width, height: image.height }
       const detectionWithLandmarks = await faceapi
@@ -28,16 +31,35 @@ const start = async () => {
       .withFaceLandmarks()
       const resizedResults = await faceapi.resizeResults(detectionWithLandmarks, displaySize)
       const align = await resizedResults.landmarks.getRefPointsForAlignment()
+      align.pop(); // dont need the mouth
+
+      if (!lefteye) lefteye = align[0];
+      if (!eyedistance) eyedistance = align[1].x - align[0].x;
+      const alignhelper = {
+        dx: lefteye.x - align[0].x * (eyedistance / (align[1].x - align[0].x)),
+        dy: lefteye.y - align[0].y * (eyedistance / (align[1].x - align[0].x)),
+        zoom: eyedistance / (align[1].x - align[0].x)
+      }
+
+      console.log('alignhelper', alignhelper)
+      console.log('eyedistance', eyedistance)
 
       const ctx = canvas.getContext('2d');
-      align.pop();
-      align.forEach(a => {
-        ctx.beginPath();
-        ctx.fillStyle = '#FF0000';
-        ctx.arc(a.x, a.y, 20, 0, 2 * Math.PI);
-        ctx.fill();
-        ctx.stroke();
-      });
+      const ctx2 = canvas.getContext('2d');
+      // align.forEach(a => {
+      //   ctx.beginPath();
+      //   ctx.fillStyle = '#FF0000';
+      //   ctx.arc(a.x, a.y, 20, 0, 2 * Math.PI);
+      //   ctx.fill();
+      //   ctx.stroke();
+      // });
+      // ctx.clearRect(0, 0, canvas.width, canvas.height);  // clear canvas
+      // ctx.drawImage(canvas2, lefteye.x - align[0].x, lefteye.y - align[0].y);
+      // ctx.drawImage(canvas, alignhelper.dx, alignhelper.dy);
+      ctx.setTransform(alignhelper.zoom, 0, 0, alignhelper.zoom, alignhelper.dx, alignhelper.dy);
+      ctx.drawImage(canvas, 0, 0);
+
+      // document.querySelector('#gif').append(canvas);
       gif.addFrame(canvas, {delay: 500});
     })
   })
